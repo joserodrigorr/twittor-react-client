18f48e7793f265fb06f2a35754117ca5966ee441
joserodrigorr@hotmail.com <joserodrigorr@hotmail.com>
2021-12-11 16:03:17 -0500

    left Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react"
+import SignInSingUp from "./page/SignInSingUp"
 
 
 export default function App() {
   const [user, setUser] = useState({name:"José"});
   
-  return <div>{user ? <h1>Estas logeado</h1>:<h1>No estas Logeado</h1>}</div>;
+  return <div>{user ? (
+    <div>
+      <SignInSingUp />
+    </div>
+  ):<h1>No estas Logeado</h1>}</div>;
 
   
 }
---
+++ src/page/SignInSingUp/SignInSingUp.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Row, Col, Button} from "react-bootstrap";
+import LogoTwittor from "../../assets/png/logo.png"
+import LogoWhiteTwittor from "../../assets/png/logo-white.png";
+
+import "./SignInSingUp.scss";
+
+export default function SignInSingUp() {
+    return (
+        <Container className='signin-signup' fluid>
+            <Row>
+                <LeftComponent />
+                <RightComponent />
+            </Row>
+
+        </Container>
+    )
+}
+
+function LeftComponent(){
+    return (
+        <Col  className='signin-signup__left' xs={6}>
+            <img src={LogoTwittor} alt="Twittor" />
+            <div>
+                <h2>- Sigue lo que te interesa.</h2>
+                <h2>- Entérate de que está hablando la gente.</h2>
+                <h2>- Únete a la conversación.</h2>
+            </div>
+        </Col>
+    )
+}
+
+function RightComponent(){
+    return (
+        <Col  className='signin-signup__right' xs={6}>
+            <h2>RightComponent...</h2>
+        </Col>
+    )
+}
