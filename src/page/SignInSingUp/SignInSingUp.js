bb3577317d9512c0e7b17afa4f5b30ac240677a6
joserodrigorr@hotmail.com <joserodrigorr@hotmail.com>
2021-12-12 12:40:43 -0500

    Termino lado izquierdo

--- src/page/SignInSingUp/SignInSingUp.js
+++ src/page/SignInSingUp/SignInSingUp.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Button} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus, faUsers, faComments} from "@fortawesome/free-solid-svg-icons";
 import LogoTwittor from "../../assets/png/logo.png"
 import LogoWhiteTwittor from "../../assets/png/logo-white.png";
 
@@ -22,9 +24,18 @@ function LeftComponent(){
         <Col  className='signin-signup__left' xs={6}>
             <img src={LogoTwittor} alt="Twittor" />
             <div>
-                <h2>- Sigue lo que te interesa.</h2>
-                <h2>- Entérate de que está hablando la gente.</h2>
-                <h2>- Únete a la conversación.</h2>
+                <h2>
+                    <FontAwesomeIcon icon={faSearchPlus} />
+                      Sigue lo que te interesa.
+                </h2>
+                <h2>
+                    <FontAwesomeIcon icon={faUsers} />
+                    Entérate de que está hablando la gente.
+                </h2>
+                <h2>
+                    <FontAwesomeIcon icon={faComments} />
+                     Únete a la conversación.
+                </h2>
             </div>
         </Col>
     )
