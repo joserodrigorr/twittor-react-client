ac928583472d6613daf62e9c5d3d5a40fde2a049
joserodrigorr@hotmail.com <joserodrigorr@hotmail.com>
2022-01-07 18:40:04 -0500

    Pagina de Error

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { map } from "lodash";
 import configRouting from "./configRouting";
 
-
 export default function Routing(props) {
-  const {setRefreshCheckLogin} = props;
+  const { setRefreshCheckLogin } = props;
+
   return (
     <Router>
       <Switch>
-          {map(configRouting, (route, index) => (
-           <Route key={index} path={route.path} exact={route.exact}>
-               <route.page  setRefreshCheckLogin = {setRefreshCheckLogin} />
-            </Route>
-         ))}
+        {map(configRouting, (route, index) => (
+          <Route key={index} path={route.path} exact={route.exact}>
+            <route.page setRefreshCheckLogin={setRefreshCheckLogin} />
+          </Route>
+        ))}
       </Switch>
-    </Router>    
+    </Router>
   );
 }
-
-
