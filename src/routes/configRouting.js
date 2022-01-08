fd13649fbc316f3800329a6ba47c41abc8fe9b30
joserodrigorr@hotmail.com <joserodrigorr@hotmail.com>
2022-01-08 11:56:38 -0500

    Antes Despliegue

--- src/routes/configRouting.js
+++ src/routes/configRouting.js
@@ -3,7 +3,13 @@ import User from "../page/User";
 import Users from "../page/Users";
 import Error404 from "../page/Error404";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
+  {
+    path: "/",
+    exact: true,
+    page: Home,
+  },  
   {
     path: "/users",
     exact: true,
@@ -13,14 +19,9 @@ export default [
     path: "/:id",
     exact: true,
     page: User,
-  },
-  {
-    path: "/",
-    exact: true,
-    page: Home,
-  },
+  },  
   {
     path: "*",
     page: Error404,
-  },
+  }, 
 ];
